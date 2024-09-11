5fce667796b1c08b6d839f66d54aa5f3b7a4e03c
Jasmeen2909 <jasmeenkaur2903@gmail.com>
2024-09-11 18:44:47 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -1,35 +1,37 @@
-const express = require("express")
-const app=express()
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-const authRoute = require("./routes/auth")
-const orderRoute = require("./routes/order")
-const menuitemRoute = require("./routes/menuitem")
-const reservationRoute = require("./routes/reservation")
-const cors=require('cors');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const orderRoute = require("./routes/order");
+const menuitemRoute = require("./routes/menuitem");
+const reservationRoute = require("./routes/reservation");
+const cors = require("cors");
 
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-.then(()=>{
-    console.log("DB Connection Successfull")
-})
-.catch((err)=>{
-    console.error("error connecting to mongoDB ",err)
-})
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successful");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
 
-//middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRoute)
-app.use("/api/order", orderRoute)
-app.use("/api/menuitem", menuitemRoute)
-app.use("/api/reservation", reservationRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/order", orderRoute);
+app.use("/api/menuitem", menuitemRoute);
+app.use("/api/reservation", reservationRoute);
 
+// Port configuration
+const port = process.env.PORT || 8800;
 
-
-app.listen(8800,()=>{
-    console.log("Backend server is running!");
+app.listen(port, () => {
+  console.log(`Backend server is running on port ${port}!`);
 });
